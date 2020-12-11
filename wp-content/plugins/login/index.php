efd9d2b12d18bdecb5aad0e6c77f8ef5b614109e
RafaelRosaSPgit config --global user.name RafaelRosaSPgit config --global user.username RafaelRosaSP <rafael@pilotomaster.com.br>
2020-12-11 15:04:42 -0300

    carregar css e js

--- wp-content/plugins/login/index.php
+++ wp-content/plugins/login/index.php
@@ -3,5 +3,37 @@
 Plugin Name: Login e Cadastro
 Description: Plugin de login e cadastro de usuarios
 Version: 1.0.0
-Author Name: Rafael Rosa
+Author: Rafael Rosa
+Description: Um Plugin de Cadsatro de Usuarios e Login
 */
+
+if(!function_exists('add_action')){
+    echo "O plugin não pode ser acessado diretamente;
+    exit;
+}
+
+// Ativação de plugin
+function dl_ativacao_plugin(){
+    if(version_compare(get_bloginfo('version, '4.8', '<')){
+        wp_die("Você precisa atualizar o wordpress para atualizar o plugin");
+    }
+}
+
+register_activation_hook(__FILE__, 'dl_ativacao_plugin');
+
+// Carregar css e js
+function carregar_js_css(){
+   wp_enqueue_style('css', plugins_url('/style.css', __FILE__));
+   wp_enqueue_script('js', plugins_url('/script.js', __FILE__), array('jquery'), '1.0', true);
+
+}
+
+add_action('wp_enqueue_script', 'carrregar_js_css');
+
+
+
+
+
+
+
+
